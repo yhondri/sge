4fd456a96013c89b46507368c1aafdc57548f0fb
Nerea <jguillen.nereajimenez@gmail.com>
2020-05-06 19:52:29 +0200

    cosos

--- p4/src/ServiceManager.java
+++ p4/src/ServiceManager.java
@@ -15,23 +15,27 @@ public class ServiceManager {
     final XmlRpcClientConfigImpl start_config = new XmlRpcClientConfigImpl();
 
     private URL getServerURL() throws MalformedURLException {
-        return new URL("https://demo.odoo.com/start");
+        return new URL("http://localhost:8069");
     }
 
     public List<Object> getProductsFromOdoo() throws XmlRpcException, MalformedURLException {
         URL serverURL = getServerURL();
         start_config.setServerURL(serverURL);
-        final Map<String, String> info = (Map<String, String>) client.execute(start_config, "start", emptyList());
+      /*  final Map<String, String> info = (Map<String, String>) client.execute(start_config, "start", emptyList());
 
         final String url = info.get("host"),
                 db = info.get("database"),
                 username = info.get("user"),
                 password = info.get("password");
-
+*/
+        String url = "http://localhost:8069";
         final XmlRpcClientConfigImpl common_config = new XmlRpcClientConfigImpl();
         common_config.setServerURL(new URL(String.format("%s/xmlrpc/2/common", url)));
 
-        client.execute(common_config, "version", emptyList());
+        String db = "ChocoCola";
+        String username = "marito07@ucm.es";
+        String password = "odoo";
+
 
         int uid = (int)client.execute(
                 common_config, "authenticate", asList(
@@ -47,13 +51,13 @@ public class ServiceManager {
         final List ids = asList((Object[])models.execute(
                 "execute_kw", asList(
                         db, uid, password,
-                        "res.product_product", "search")));
+                        "product.product", "search", asList())));
 
         // TODO: Es probable que tengamos que listar los campos que queremos.
         final List<Object> record = asList((Object[])models.execute(
                 "execute_kw", asList(
                         db, uid, password,
-                        "res.product_product", "read",
+                        "product.product", "read",
                         asList(ids)
                 )
         ));
